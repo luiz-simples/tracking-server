e75bbd476e9ea5379ce00f2e52df15fa3591230c
Luiz Amorim <luiz.simples@gmail.com>
2016-03-03 11:17:31 -0300

    Adust universal tracking-analytics.js

--- release/tracking-analytics.js
+++ release/tracking-analytics.js
@@ -1,15 +1,23 @@
-/*globals window:false, document:false */
-;(function(window, document) {
-  // Strict Mode
+;(function (global, factory) {
+  var isModule = typeof exports === 'object' && typeof module !== 'undefined';
+  var isAMD    = typeof define === 'function' && define.amd;
+  var isGlobal = !isModule && !isAMD;
+
+  if (isAMD) define(factory);
+  if (isModule) module.exports = factory();
+  if (isGlobal) global.trackingAnalytics = factory();
+}(this, function () {
   'use strict';
 
   var trackingAnalytics = {
     sendActivity: function(name, reference) {
       console.log('SUCESSO::', name, reference);
+    },
+
+    sendContact: function(name, reference) {
+      console.log('SUCESSO::', name, reference);
     }
   };
 
-  // Tracking Analytics
-  window['ta'] = trackingAnalytics;
-  window['trackingAnalytics'] = trackingAnalytics;
-})(window, document);
+  return trackingAnalytics;
+}));

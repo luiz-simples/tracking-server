48669b9c33329a314ba47ce2bc8dd31f8c2baf8f
Luiz Amorim <luiz.simples@gmail.com>
2016-03-03 12:07:13 -0300

    vanillaJS manipulate cookie

--- release/tracking-analytics.js
+++ release/tracking-analytics.js
@@ -17,12 +17,60 @@
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
 
+  function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+  // Create cookie
+  function createCookie(name, value, days) {
+      var expires;
+      if (days) {
+          var date = new Date();
+          date.setTime(date.getTime()+(days*24*60*60*1000));
+          expires = "; expires="+date.toGMTString();
+      }
+      else {
+          expires = "";
+      }
+      document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+  // Read cookie
+  function readCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+          var c = ca[i];
+          while (c.charAt(0) === ' ') {
+              c = c.substring(1,c.length);
+          }
+          if (c.indexOf(nameEQ) === 0) {
+              return c.substring(nameEQ.length,c.length);
+          }
+      }
+      return null;
+  }
+
+  // Erase cookie
+  function eraseCookie(name) {
+      createCookie(name,"",-1);
+  }
+
+  var cookieName = 'tracking-server-cid';
+  createCookie(cookieName, guid());
+
   var trackingAnalytics = {
     sendActivity: function(name, reference) {
-      console.log(document.cookie);
+      var cookieId = readCookie(cookieName);
+      console.log(cookieId);
       xhttp.open("POST", "//tracking-server.herokuapp.com/activities", true);
       xhttp.setRequestHeader("Content-type", "application/json");
-      xhttp.send(JSON.stringify({ name: name, reference: reference, cid: 'sdladklasd', hid: 'kajskdjasd' }));
+      xhttp.send(JSON.stringify({ name: name, reference: reference, cid: cookieId, hid: 'kajskdjasd' }));
       console.log('SUCESSO::', name, reference);
     },
 

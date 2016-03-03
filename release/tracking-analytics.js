043d7f7f2ac42e8e89c85f6330dea8d2c8aa354b
Luiz Amorim <luiz.simples@gmail.com>
2016-03-03 15:15:51 -0300

    Ajusts View page

--- release/tracking-analytics.js
+++ release/tracking-analytics.js
@@ -80,6 +80,7 @@
 
     sendContact: function(name, email, message, callAfterLoad) {
       var xhttp = getHttpRequest();
+
       xhttp.addEventListener("load", function(evt) {
         var targetResponse = evt.currentTarget;
         var isError = Boolean(targetResponse.status >= 400);
@@ -91,6 +92,7 @@
 
         callAfterLoad(messageError, result);
       });
+
       xhttp.open("POST", "//tracking-server.herokuapp.com/contacts");
       xhttp.setRequestHeader("Content-type", "application/json");
       xhttp.send(JSON.stringify({ name: name, email: email, message: message, cid: cookieId, hid: hostId }));
